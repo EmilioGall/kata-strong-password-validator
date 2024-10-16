6b5309ce0e011e50eabb71f1e6e01798c5b91b28
Emilio Gallo <emilio.gallo17@gmail.com>
2024-10-16 19:10:46 +0200

    feat: added function controlPassword( ) - functions.js

--- js/functions.js
+++ js/functions.js
@@ -77,7 +77,7 @@ function printProgressBar(requestsArray, progressBar) {
    if (progressPercentage <= 20) {
 
       barColor = 'danger';
-      
+
    } else if (progressPercentage > 20 && (progressPercentage <= 80)) {
 
       barColor = 'warning';
@@ -197,6 +197,90 @@ function countSpecialChar(str) {
    };
 
    return count;
+
+};
+
+function controlPassword(listenerType) {
+
+   // Define constant of Password Input Value
+   const inputPasswordValue = inputPasswordElem.value.trim();
+
+   console.log("inputPasswordValue:", inputPasswordValue, typeof inputPasswordValue);
+
+   if (!inputPasswordValue) {
+
+      console.log('empty');
+
+      requestsArray.forEach((request, i) => {
+
+         request.status = listenerType == 'input' ? 'default' : 'failed';
+
+      });
+
+   } else {
+
+      // Control of Requirement 1
+      if (countWhitespace(inputPasswordValue) <= requestsArray[0].requestValue) {
+
+         requestsArray[0].status = 'validated';
+
+      } else if (countWhitespace(inputPasswordValue) > requestsArray[0].requestValue) {
+
+         requestsArray[0].status = listenerType == 'input' ? 'default' : 'failed';
+
+      };
+
+      // Control of Requirement 2
+      if (inputPasswordValue.length >= requestsArray[1].requestValue) {
+
+         requestsArray[1].status = 'validated';
+
+      } else if (inputPasswordValue.length < requestsArray[1].requestValue) {
+
+         requestsArray[1].status = listenerType == 'input' ? 'default' : 'failed';
+
+      };
+
+      // Control of Requirement 3
+      if (countUpperCase(inputPasswordValue) >= requestsArray[2].requestValue) {
+
+         requestsArray[2].status = 'validated';
+
+      } else if (countUpperCase(inputPasswordValue) < requestsArray[2].requestValue) {
+
+         requestsArray[2].status = listenerType == 'input' ? 'default' : 'failed';
+
+      };
+
+      // Control of Requirement 4
+      if (countNumbers(inputPasswordValue) >= requestsArray[3].requestValue) {
+
+         requestsArray[3].status = 'validated';
+
+      } else if (countNumbers(inputPasswordValue) < requestsArray[3].requestValue) {
+
+         requestsArray[3].status = listenerType == 'input' ? 'default' : 'failed';
+
+      };
+
+      // Control of Requirement 5
+      if (countSpecialChar(inputPasswordValue) >= requestsArray[4].requestValue) {
+
+         requestsArray[4].status = 'validated';
+
+      } else if (countSpecialChar(inputPasswordValue) < requestsArray[4].requestValue) {
+
+         requestsArray[4].status = listenerType == 'input' ? 'default' : 'failed';
+
+      };
+
+   };
+
+   // Print on [requestsListElem] requests in [requestsArray]
+   printListRequest(requestsArray, requestsListElem);
+
+   // Print on [progressBarElem] requests status completion
+   printProgressBar(requestsArray, progressBarElem);
 
 };
 
