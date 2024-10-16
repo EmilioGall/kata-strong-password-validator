4e7bde45e9e3f1b914ff552f47df1b3682b36520
Emilio Gallo <emilio.gallo17@gmail.com>
2024-10-16 12:25:52 +0200

    feat: created constants for DOM Elements and array of requests - elements.js

---
+++ js/elements.js
@@ -0,0 +1,61 @@
+console.log('///// Elements /////');
+
+// Define constant of Password Input Dom Element
+const requestsArray = [
+
+   {
+      requestValue: 9,
+      requestText: function () {
+
+         return `Minimum <b>${this.requestValue} characters</b>`;
+
+      },
+   },
+   {
+      requestValue: 1,
+      requestText: function () {
+
+         return `At least <b>${this.requestValue} capital (uppercase) letter</b>`;
+
+      },
+   },
+   {
+      requestValue: 1,
+      requestText: function () {
+
+         return `</i>Minimum <b>${this.requestValue} number</b>`;
+
+      },
+   },
+   {
+      requestValue: 2,
+      requestText: function () {
+
+         return `At least <b>${this.requestValue} special character</b>`;
+
+      },
+   },
+
+];
+
+requestsArray.forEach((request, i) => {
+
+   console.log(`Request n.${i+1} =`, request.requestText());
+
+});
+
+
+// Define constant of Password Input Dom Element
+const inputPasswordElem = document.querySelector("#input-password");
+
+console.log("inputPasswordElem", typeof inputPasswordElem, inputPasswordElem);
+
+// Define constant of List of Requests Dom Element
+const requestsListElem = document.querySelector("#requests-list");
+
+console.log("requestsListElem", typeof requestsListElem, requestsListElem);
+
+// Define constant of Progress Bar Dom Element
+const progressBarElem = document.querySelector("#progress-bar");
+
+console.log("progressBarElem", typeof progressBarElem, progressBarElem);
